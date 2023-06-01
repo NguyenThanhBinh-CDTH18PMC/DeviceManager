46278a738f75a925fa8168b635e75b3572611b08
0306181206CanNguyenAndroid <75105000+0306181206CanNguyenAndroid@users.noreply.github.com>
2023-06-01 22:32:43 +0700

    Test: Create QR Code

---
+++ DeviceManagerApp/DeviceManagerApp/DAO/QRCodeWriter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using ZXing;
+using ZXing.Common;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using ZXing.QrCode.Internal;
+
+namespace DeviceManagerApp.DAO
+{
+    class QRCodeWriterClass
+    {
+        public static Bitmap GenerateQRCode(string content, int width, int height)
+        {
+            var options = new EncodingOptions
+            {
+                Width = width,
+                Height = height,
+                Margin = 0
+            };
+
+            var writer = new BarcodeWriterPixelData
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = options
+            };
+
+            var pixelData = writer.Write(content);
+            var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppRgb);
+
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+            Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+            bitmap.UnlockBits(bitmapData);
+
+            return bitmap;
+        }
+
+        /*
+        public void Create_QR(String Device)
+        {
+
+            // Create an instance of the BarcodeWriter class
+            var barcodeWriter = new BarcodeWriter();
+
+            // Set the format of the barcode writer to QR_CODE
+            barcodeWriter.Format = BarcodeFormat.QR_CODE;
+
+            // Create an instance of the QRCodeWriter class
+            var writer = new BarcodeWriter();
+
+            // Create an instance of the BitMatrix class
+            var matrix = writer.Encode(Device);
+
+           
+
+            // Set the options of the barcode writer
+            var options = new EncodingOptions
+            {
+                Margin = 1,
+                ErrorCorrection = ErrorCorrectionLevel.H
+            };
+            barcodeWriter.Options = options;
+
+            // Create a bitmap image
+            var bitmap = barcodeWriter.Write(matrix);
+
+            // Save the bitmap image to a file
+            bitmap.Save("qrcode.png");
+        }
+        */
+    }
+}
---
+++ DeviceManagerApp/DeviceManagerApp/QR_Code.cs
@@ -0,0 +1,31 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DeviceManagerApp
+{
+    public partial class QR_Code : Form
+    {
+        public QR_Code()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random R = new Random();
+            int QR = R.Next(100, 1000);
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("Thiet bi " + QR, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(5);
+
+            ptb_show.Image = qrCodeImage;
+        }
+    }
+}
